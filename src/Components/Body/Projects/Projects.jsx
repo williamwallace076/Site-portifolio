cb8c619d79899bd01345b44db865e22d54436425
williamwallace076 <wallacewilliam076@gmail.com>
2023-07-29 16:53:30 -0300

    feat:images_response_added

--- src/Components/Body/Projects/Projects.jsx
+++ src/Components/Body/Projects/Projects.jsx
@@ -10,15 +10,33 @@ import eComeceSmall from "../../../assets/imgs/e-comerceSmall.jpg"
 
 
 import thumbNetflix from "../../../assets/imgs/netflix.png";
-import netflixDesktop from "../../../assets/imgs/netflix.png"
-import netflixMid from "../../../assets/imgs/netflix-mid.png"
-import netflixSmall from "../../../assets/imgs/netflix-small.png"
+import netflixDesktop from "../../../assets/imgs/netflix.png";
+import netflixMid from "../../../assets/imgs/netflix-mid.png";
+import netflixSmall from "../../../assets/imgs/netflix-small.png";
 
 import thumbSpotfy from "../../../assets/imgs/spotfy.png";
+import spotPremiumDesktop from "../../../assets/imgs/spotPremium-desktop.png";
+import spotPremiumMid from "../../../assets/imgs/spotPremium-mid.png";
+import spotPremiumSmall from "../../../assets/imgs/spotPremium-mid.png";
+
 import thumbFlipBird from "../../../assets/imgs/flipbird.png";
+
+
 import thumbPokedex from "../../../assets/imgs/pokedex.png";
+import pokedexDesktop from "../../../assets/imgs/pokedex-desktop.png";
+import pokedexMid from "../../../assets/imgs/pokedex-desktop.png";
+import pokedexSmall from "../../../assets/imgs/pokedex-desktop.png";
+
 import thumbTodolist from "../../../assets/imgs/todolist.png";
+import todolistDesktop from "../../../assets/imgs/todolist.png";
+import todolistMid from "../../../assets/imgs/todolist.png";
+import todolistSmall from "../../../assets/imgs/todolist.png";
+
+
 import thumbMarvel from "../../../assets/imgs/Marvel.png";
+import marvelSelectDesktop from "../../../assets/imgs/marvelSelect-desktop.png";
+import marvelSelectMid from "../../../assets/imgs/marvelSelect-desktop.png";
+import marvelSelectSmall from "../../../assets/imgs/marvelSelect-desktop.png";
 
 
 import {faSquareJs,faReact,faHtml5, faCss3Alt} from "@fortawesome/free-brands-svg-icons";
@@ -63,19 +81,69 @@ const Projects = () => {
                 <AreaProjectItem>
 
                 <ProjectItem classname="mid" title="Spotfy Clone" subtitle="Clone da Landing Page de Planos Do Spotfy" 
-                link="https://spotify-clone-eta-three.vercel.app/" thumbnail={thumbSpotfy} tec1={faReact} tec2={faHtml5} tec3={faCss3Alt} tec4={faSquareJs} ></ProjectItem>
+                link="https://spotify-clone-eta-three.vercel.app/" thumbnail={thumbSpotfy} tec1={faReact} tec2={faHtml5} tec3={faCss3Alt} tec4={faSquareJs} 
+                details={{
+                  title: "Spotfy Clone",
+                  desc: "Landing page clone da Página de Planos Premium do Spotfy. ",
+                  img_desktop: spotPremiumDesktop,
+                  img_mid: spotPremiumMid,
+                  img_small: spotPremiumSmall,
+                  websiteLink: "https://spotify-clone-eta-three.vercel.app/",
+                  repositoryLink: "https://github.com/williamwallace076/spotify-clone",
+                  id: 3,
+               }} ></ProjectItem>
 
                 <ProjectItem classname="mid" title="Flappy Bird" subtitle="Jogo Flappy Bird Com Lógica em JS" 
-                link="https://flappybirdclone.vercel.app/" thumbnail={thumbFlipBird} tec1={faHtml5} tec2={faCss3Alt} tec3={faSquareJs} ></ProjectItem>
+                link="https://flappybirdclone.vercel.app/" thumbnail={thumbFlipBird} tec1={faHtml5} tec2={faCss3Alt} tec3={faSquareJs} 
+                 details={{
+                        title: "Netflix Clone",
+                        desc: "Clone do Site Netflix com integração da Api 'theMovieDb'.",
+                        img_desktop: netflixDesktop,
+                        img_mid: netflixMid,
+                        img_small: netflixSmall,
+                        websiteLink: "https://netflix-clone-swart-sigma.vercel.app/",
+                        repositoryLink: "https://github.com/williamwallace076/Netflix-clone",
+                        id: 4,
+                     }} ></ProjectItem>
 
                 <ProjectItem classname="mid" title="Pokedex" subtitle="Lista de Cards de Pokemons" 
-                link="https://williamwallace076.github.io/Pokedex/" thumbnail={thumbPokedex} tec1={faHtml5} tec2={faCss3Alt} tec3={faSquareJs} ></ProjectItem>
+                link="https://williamwallace076.github.io/Pokedex/" thumbnail={thumbPokedex} tec1={faHtml5} tec2={faCss3Alt} tec3={faSquareJs} 
+                 details={{
+                        title: "Pokedex",
+                        desc: "Pokedex de listagem de pokemons, projeto do curso DevWeek.",
+                        img_desktop: pokedexDesktop,
+                        img_mid: pokedexMid,
+                        img_small: pokedexSmall,
+                        websiteLink: "https://williamwallace076.github.io/Pokedex/",
+                        repositoryLink: "https://williamwallace076.github.io/Pokedex/",
+                        id: 5,
+                     }} ></ProjectItem>
 
                 <ProjectItem classname="mid" title="List de Tarefas" subtitle="Gerenciamento de tarefas pessoais"
-                 link="https://todolist-seven-delta.vercel.app/" thumbnail={thumbTodolist} tec1={faReact} tec2={faHtml5} tec3={faCss3Alt} tec4={faSquareJs} ></ProjectItem>
+                 link="https://todolist-seven-delta.vercel.app/" thumbnail={thumbTodolist} tec1={faReact} tec2={faHtml5} tec3={faCss3Alt} tec4={faSquareJs} 
+                  details={{
+                        title: "ToDo List",
+                        desc: "Lista de tarefas, utilizando o gerenciamento de estados com React.",
+                        img_desktop: todolistDesktop,
+                        img_mid: todolistMid,
+                        img_small: todolistSmall,
+                        websiteLink: "https://todolist-seven-delta.vercel.app/",
+                        repositoryLink: "https://github.com/williamwallace076/TodoList",
+                        id: 6,
+                     }} ></ProjectItem>
                
                <ProjectItem classname="mid" title="Seleção de personagem" subtitle="Menu de seleção de personagem Marvel"
-                 link="https://marvelpelectionplayer.vercel.app/" thumbnail={thumbMarvel} tec1={faHtml5} tec2={faCss3Alt} tec3={faSquareJs}></ProjectItem>
+                 link="https://marvelpelectionplayer.vercel.app/" thumbnail={thumbMarvel} tec1={faHtml5} tec2={faCss3Alt} tec3={faSquareJs}
+                  details={{
+                        title: "Seleção de personagens Marvel",
+                        desc: "Simuldador do menu de seleção de personagens da marvel",
+                        img_desktop: marvelSelectDesktop,
+                        img_mid: marvelSelectMid,
+                        img_small: marvelSelectSmall,
+                        websiteLink: "https://marvelpelectionplayer.vercel.app/",
+                        repositoryLink: "https://github.com/williamwallace076/MarvelSelectionPlayer",
+                        id: 7,
+                     }} ></ProjectItem>
 
 
                 </AreaProjectItem>
